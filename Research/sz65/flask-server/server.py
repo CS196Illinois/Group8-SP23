25f209ebe19c062261efe212d2538bdceacd087e
Realsumen <realsumen001@163.com>
2023-04-08 20:45:43 -0500

    Add simple post and get methods to the server

--- Research/sz65/flask-server/server.py
+++ Research/sz65/flask-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_file, json, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,6 +9,17 @@ CORS(app)
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
 
+@app.route("/mainpage", methods=['Get'])
+def mainpage():
+    return {
+        send_file("page.html")
+    }
+
+@app.route("/posttest", methods=['Post'])
+def posttest():
+    request_data = json.loads(request.data)
+    return request_data['age']
+    
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
 
