4462902de6cddb5e034249ba3b34ba25297f7561
WilliamWang <wangwilliam831@gmail.com>
2023-04-16 14:43:20 -0500

    changes the server

--- Project/back/server.py
+++ Project/back/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, send_file, json, jsonify
 from flask_cors import CORS
+import functions;
 
 app = Flask(__name__)
 CORS(app)
@@ -19,9 +20,33 @@ def mainpage():
 def posttest():
     request_data = json.loads(request.data)
     return request_data['age']
+
+def calculate_interest():
+    # Get the request parameters
+    p = float(request.form['p'])
+    r = float(request.form['r'])
+    t = float(request.form['t'])
+    interest_type = request.form['interest_type']
+
+    if interest_type == 'simple':
+        result = functions.simple_interest(p, r, t)
+    elif interest_type == 'compound':
+        result = functions.compound_interest(p, r, t)
+    elif interest_type == 'CAGR':
+        a = float(request.form['a'])
+        result = functions.CAGR(p, a, t)
+    elif interest_type == 'double':
+        result = functions.double(r)
+    elif interest_type == 'pur_power':
+        result = functions.pur_power(p, r, t)
+    else:
+        return 'Invalid interest type'
+
+    return str(result)
     
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
+
 
 # @app.route("/add", methods=["POST"], strict_slashes=False)
 # def add_articles():
