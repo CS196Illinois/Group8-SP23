3ff28cdbf0483b5b7eb8368d307bf362732e1a3a
Realsumen <realsumen001@163.com>
2023-04-16 22:09:42 -0500

    fixing the background pic issue

--- Project/Client/src/Pages/PostForm.js
+++ Project/Client/src/Pages/PostForm.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-// import './resources/calc.css';
+import './resources/calc.css';
 
 class PostForm extends Component {
+
     constructor(props) {
         super(props)
         this.state = {
@@ -12,6 +13,13 @@ class PostForm extends Component {
             type: '',
             interest: ''
         }
+        const div = document.createElement('div');
+        div.style.backgroundImage = 'url("https://wallpapercave.com/wp/wp5169135.jpg")';
+        div.style.backgroundSize = 'cover';
+        div.style.backgroundPosition = 'center';
+        div.style.opacity = 0.5;
+        document.body.appendChild(div);
+
     }
 
     changeHandler = (e) => {
@@ -34,17 +42,15 @@ class PostForm extends Component {
     render() {
         const { p, r, t, type } = this.state;
         return (
-            <div style={{ 
+            <div className='pic'
+                style={{
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
-                backgroundImage: 'url("uiuc.jpg")',
-                backgroundSize: 'cover',
-                height: '100vh',
-              }}>
-                <h1 style={{ marginBottom: '2rem' }}>Interest Calculator</h1>
-                <form onSubmit={this.submitHandler} style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+            }}>
+                <h1 style={{ fontFamily: 'Arial', fontSize: '4rem', marginBottom: '2rem' }}>Interest Calculator</h1>
+                <form onSubmit={this.submitHandler} style={{fontSize:'2rem', display: 'flex', flexDirection: 'column', gap: '1.5rem' }}>
+                    <div style={{ fontFamily: 'Arial', display: 'flex', flexDirection: 'column' }}>
                         <label style={{ marginBottom: '0.5rem' }}>Deposit:</label>
                         <input type="text" name="p" value={p} onChange={this.changeHandler} />
                     </div>
@@ -60,7 +66,7 @@ class PostForm extends Component {
                         <label style={{ marginBottom: '0.5rem' }}>Deposit Type:</label>
                         <input type="text" name="type" value={type} onChange={this.changeHandler} />
                     </div>
-                    <button type="submit" style={{ marginTop: '1rem', padding: '0.5rem 1rem' }}>
+                    <button type="submit" style={{ fontFamily: 'Arial', marginTop: '1rem', padding: '0.5rem 1rem' }}>
                         Calculate
                     </button>
                 </form>
